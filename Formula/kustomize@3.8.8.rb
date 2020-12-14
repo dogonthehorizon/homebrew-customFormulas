4679285ec2c88d339f82060e5ca80605a1b777fc
Fernando Freire <fernando.freire@armory.io>
2020-12-14 15:22:53 -0800

    Add kustomize 3.8.8

---
+++ Formula/kustomize@3.8.8.rb
@@ -0,0 +1,67 @@
+class KustomizeAT388 < Formula
+  desc "Template-free customization of Kubernetes YAML manifests"
+  homepage "https://github.com/kubernetes-sigs/kustomize"
+  url "https://github.com/kubernetes-sigs/kustomize.git",
+      tag:      "kustomize/v3.8.8",
+      revision: "72262c5e7135045ed51b01e417a7e72f558a22b0"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes-sigs/kustomize.git"
+
+  livecheck do
+    url :head
+    regex(%r{kustomize/v?(\d+(?:\.\d+)+)$}i)
+  end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "62647fe5a7b1cce568d57769041a5abc07e2f02434d44d90a332b85ef830504d" => :big_sur
+    sha256 "2395b667c616f38313bc5e57c82e9b6466026700a86e8ed27516a8e7a59bd41e" => :catalina
+    sha256 "22ab541f81a10f1c381e0ebc1da5477ebf93e4935720482107e4ef4b84c9baa6" => :mojave
+  end
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.safe_popen_read("git", "rev-parse", "HEAD").strip
+    tag = Utils.safe_popen_read("git", "tag", "--contains", "HEAD").strip
+
+    cd "kustomize" do
+      ldflags = %W[
+        -s -X sigs.k8s.io/kustomize/api/provenance.version=#{tag}
+        -X sigs.k8s.io/kustomize/api/provenance.gitCommit=#{revision}
+        -X sigs.k8s.io/kustomize/api/provenance.buildDate=#{Time.now.iso8601}
+      ]
+      system "go", "build", "-ldflags", ldflags.join(" "), "-o", bin/"kustomize"
+    end
+  end
+
+  test do
+    assert_match "kustomize/v#{version}", shell_output("#{bin}/kustomize version")
+
+    (testpath/"kustomization.yaml").write <<~EOS
+      resources:
+      - service.yaml
+      patchesStrategicMerge:
+      - patch.yaml
+    EOS
+    (testpath/"patch.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        selector:
+          app: foo
+    EOS
+    (testpath/"service.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        type: LoadBalancer
+    EOS
+    output = shell_output("#{bin}/kustomize build #{testpath}")
+    assert_match /type:\s+"?LoadBalancer"?/, output
+  end
+end
