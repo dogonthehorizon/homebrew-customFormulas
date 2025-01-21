6808533eefc87d006a0e779676613d0ce0047809
Fernando Freire <dogonthehorizon@gmail.com>
2025-01-21 14:30:43 -0800

    feat: add pgformatter built from main instead of tags

---
+++ Formula/pgformatter@main.rb
@@ -0,0 +1,29 @@
+class Pgformatter < Formula
+  desc "PostgreSQL syntax beautifier"
+  homepage "https://sqlformat.darold.net/"
+  url "https://github.com/darold/pgFormatter/archive/refs/heads/master.tar.gz"
+  sha256 "b427041dd6754b1a77d411590abc4d729bf508ba1d6f3e15ade07e825af33fa5"
+  license "PostgreSQL"
+
+  def install
+    system "perl", "Makefile.PL", "DESTDIR=."
+    system "make", "install"
+
+    if OS.linux?
+      # Move man pages to share directory so they will be linked correctly on Linux
+      mkdir "usr/local/share"
+      mv "usr/local/man", "usr/local/share"
+    end
+
+    prefix.install (buildpath/"usr/local").children
+    (libexec/"lib").install "blib/lib/pgFormatter"
+    libexec.install bin/"pg_format"
+    bin.install_symlink libexec/"pg_format"
+  end
+
+  test do
+    test_file = (testpath/"test.sql")
+    test_file.write("SELECT * FROM foo")
+    system bin/"pg_format", test_file
+  end
+end
